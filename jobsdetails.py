2d00502e0634b896bfa7fe61de2eb79e293b46cd
Nilakshi23 <rao.nilaxshi@gmail.com>
2022-02-02 23:53:49 +0530

    three

--- jobsdetails.py
+++ jobsdetails.py
@@ -1,9 +1,15 @@
 from jenkinsapi.jenkins import Jenkins
+
+def get_server_instance():
+    jenkins_url = 'http://jenkins_host:8080'
+    server = Jenkins(jenkins_url, username='nilaxshi', password='1234')
+    return server
+    
 def get_job_details():
     # Refer Example #1 for definition of function 'get_server_instance'
     server = get_server_instance()
     for job_name, job_instance in server.get_jobs():
-        print 'Job Name:%s' % (job_instance.name)
-        print 'Job Description:%s' % (job_instance.get_description())
-        print 'Is Job running:%s' % (job_instance.is_running())
-        print 'Is Job enabled:%s' % (job_instance.is_enabled())
+        print(job_instance.name)
+        print(job_instance.get_description())
+        print(job_instance.is_running())
+        print(job_instance.is_enabled()) 
