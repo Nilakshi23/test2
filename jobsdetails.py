358bcc52393ebee0d9140aa877fcd74f75f48610
Nilakshi23 <rao.nilaxshi@gmail.com>
2022-02-03 00:09:35 +0530

    6

--- jobsdetails.py
+++ jobsdetails.py
@@ -9,9 +9,9 @@ def get_job_details():
     # Refer Example #1 for definition of function 'get_server_instance'
     server = get_server_instance()
     for job_name, job_instance in server.get_jobs():
-        print(job_instance.name)
-        print(job_instance.get_description())
-        print(job_instance.is_running())
-        print(job_instance.is_enabled()) 
+        print 'Job Name:%s' %(job_instance.name)
+        print 'Job Description:%s' %(job_instance.get_description())
+        print 'Is Job running:%s' %(job_instance.is_running())
+        print 'Is Job enabled:%s' %(job_instance.is_enabled())
 
 get_job_details()
