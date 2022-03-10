2475671800fe5dfc25743086cd2d970705afe321
Nilakshi23 <84282600+Nilakshi23@users.noreply.github.com>
2022-03-10 14:47:47 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -7,15 +7,15 @@ BUILD_NUMBER=sys.argv[3]
 BUILD_USER = sys.argv[2]
 BUILD_URL = sys.argv[4]
 BUILD_STATUS = sys.argv[5]
-SonarQubeQualityGateStatus = sys.argv[6]
+QGStatus = sys.argv[6]
 Dashboard = sys.argv[7]
-#CHATBOT = sys.argv[8]
+CHATBOT = sys.argv[8]
 
 def main():
     """Hangouts Chat incoming webhook quickstart."""
-    url = 'https://chat.googleapis.com/v1/spaces/AAAARM7dFAE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=73W5Cltjjjv3IGdUv0MWoMP8eegMavqPu3HpAYTeL5I%3D'
+    url = $CHATBOT
     bot_message = {
-        'text' : f"Job Name: {JOB_NAME}\nStarted by User: {BUILD_USER} \nBuild Number: {BUILD_NUMBER}\nBuild URL :  {BUILD_URL}\nStatus: {BUILD_STATUS}\nQuality Gate Status: {SonarQubeQualityGateStatus}\nSonar Project URL : {Dashboard}" 
+        'text' : f"Job Name: {JOB_NAME}\nStarted by User: {BUILD_USER} \nBuild Number: {BUILD_NUMBER}\nBuild URL :  {BUILD_URL}\nStatus: {BUILD_STATUS}\nQuality Gate Status: {QGStatus}\nSonar Project URL : {Dashboard}" 
 		}
 
     message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
