5e142b81a8904a7bd0a5db835d6218a52f0ee1ae
Nilakshi23 <84282600+Nilakshi23@users.noreply.github.com>
2022-03-10 19:17:56 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -11,7 +11,7 @@ CHATBOT = sys.argv[6]
 def main():
     """Hangouts Chat incoming webhook quickstart."""
    # url = 'https://chat.googleapis.com/v1/spaces/AAAARM7dFAE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=73W5Cltjjjv3IGdUv0MWoMP8eegMavqPu3HpAYTeL5I%3D'
-    url = $CHATBOT
+    url = CHATBOT
     bot_message = {
         'text' : f"Job Name: {JOB_NAME}\nStarted by User: {BUILD_USER} \nBuild Number: {BUILD_NUMBER}\nBuild URL :  {BUILD_URL}\nStatus: {BUILD_STATUS}" 
 		}
