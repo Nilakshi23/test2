81f3e7876f389a308e64dfa67a90b2aa0504f764
Nilakshi23 <84282600+Nilakshi23@users.noreply.github.com>
2022-03-10 13:24:51 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -9,11 +9,11 @@ BUILD_URL = sys.argv[4]
 BUILD_STATUS = sys.argv[5]
 SonarQubeQualityGateStatus = sys.argv[6]
 Dashboard = sys.argv[7]
-CHATBOT = sys.argv[8]
+#CHATBOT = sys.argv[8]
 
 def main():
     """Hangouts Chat incoming webhook quickstart."""
-    url = CHATBOT
+    url = 'https://chat.googleapis.com/v1/spaces/AAAARM7dFAE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=73W5Cltjjjv3IGdUv0MWoMP8eegMavqPu3HpAYTeL5I%3D\'
     bot_message = {
         'text' : f"Job Name: {JOB_NAME}\nStarted by User: {BUILD_USER} \nBuild Number: {BUILD_NUMBER}\nBuild URL :  {BUILD_URL}\nStatus: {BUILD_STATUS}\nQuality Gate Status: {SonarQubeQualityGateStatus}\nSonar Project URL : {Dashboard}" 
 		}
